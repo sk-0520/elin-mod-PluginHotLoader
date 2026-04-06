43c197e0bed61bbb13ca003573e81bdd63a75858
sk <content.type.text.net@gmail.com>
2026-04-06 19:04:28 +0900

    テンプレコピペ

---
+++ Elin.Plugin.Main/Plugin.cs
@@ -0,0 +1,32 @@
+#if DEBUG
+#endif
+using HarmonyLib;
+
+// Mod 用テンプレート組み込み想定
+
+namespace Elin.Plugin.Main
+{
+    partial class Plugin
+    {
+        #region function
+
+        /// <summary>
+        /// 起動時のプラグイン独自処理。
+        /// </summary>
+        /// <param name="harmony"></param>
+        private void AwakePlugin(Harmony harmony)
+        {
+            //NOP
+        }
+
+        /// <summary>
+        /// 終了時のプラグイン独自処理。
+        /// </summary>
+        private void OnDestroyPlugin()
+        {
+            //NOP
+        }
+
+        #endregion
+    }
+}
